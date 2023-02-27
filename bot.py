1a40fe8057b0587aaad9d28d4bc766b4c3df449a
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-27 13:48:24 -0500

    have chatgpt add a command to subscribe and unsubscribe

--- bot.py
+++ bot.py
@@ -7,12 +7,14 @@ from bs4 import BeautifulSoup
 # Set up the Discord bot
 bot = commands.Bot(command_prefix='!')
 
-# Read the bot token and channel ID from environment variables
+# Read the bot token from environment variables
 DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
-DISCORD_CHANNEL_ID = int(os.environ['DISCORD_CHANNEL_ID'])
 
 # Set up the RSS feed
 rss_url = 'https://frame.work/blog/feed.rss'
+
+# Initialize the subscribed_channels list
+subscribed_channels = []
 
 # Define a function to check for new posts
 @tasks.loop(minutes=10)
@@ -33,12 +35,24 @@ async def check_for_new_posts():
         summary = soup.get_text().strip()
         message = f'New post on the frame.work blog:\n\n**{latest_post.title}**\n{summary}\n{latest_post.link}'
 
-        # Send the Discord message to the designated channel
-        channel = bot.get_channel(DISCORD_CHANNEL_ID)
-        await channel.send(message)
+        # Send the Discord message to all subscribed channels
+        for channel_id in subscribed_channels:
+            channel = bot.get_channel(channel_id)
+            await channel.send(message)
 
 # Initialize the last_post_link attribute
 check_for_new_posts.last_post_link = None
+
+# Define a subscribe command that adds or removes channels from the subscribed_channels list
+@bot.command()
+async def subscribe(ctx):
+    channel_id = ctx.channel.id
+    if channel_id in subscribed_channels:
+        subscribed_channels.remove(channel_id)
+        await ctx.send('This channel has been unsubscribed from the frame.work blog feed.')
+    else:
+        subscribed_channels.append(channel_id)
+        await ctx.send('This channel has been subscribed to the frame.work blog feed.')
 
 # Start the task
 @bot.event
