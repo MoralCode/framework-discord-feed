11f86a06045fbd3410671074d2ef84718678044b
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-27 14:21:16 -0500

    chatgpt - persist subscriptions to a file with some light coaxing to remove a few mistakes

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import feedparser
 import discord
 from discord.ext import tasks, commands
 from bs4 import BeautifulSoup
+import json
 
 intents = discord.Intents.default() # or .all() if you ticked all, that is easier
 intents.message_content  = True # If you ticked the SERVER MEMBERS INTENT
@@ -16,8 +17,11 @@ DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
 # Set up the RSS feed
 rss_url = 'https://frame.work/blog.rss'
 
-# Initialize the subscribed_channels list
-subscribed_channels = []
+# Initialize the last_post_id attribute
+check_for_new_posts.last_post_id = None
+
+# Initialize the subscribed_channels dictionary
+subscribed_channels = {}
 
 # Define a function to check for new posts
 @tasks.loop(minutes=10)
@@ -29,9 +33,9 @@ async def check_for_new_posts():
     latest_post = feed.entries[0]
 
     # Check if the latest post has already been sent
-    if latest_post.link != check_for_new_posts.last_post_link:
-        # Save the link to the latest post
-        check_for_new_posts.last_post_link = latest_post.link
+    if latest_post.id != check_for_new_posts.last_post_id:
+        # Save the ID of the latest post
+        check_for_new_posts.last_post_id = latest_post.id
 
         # Format the post's title, summary, and URL into a Discord message
         soup = BeautifulSoup(latest_post.summary, 'html.parser')
@@ -39,29 +43,50 @@ async def check_for_new_posts():
         message = f'New post on the frame.work blog:\n\n**{latest_post.title}**\n{summary}\n{latest_post.link}'
 
         # Send the Discord message to all subscribed channels
-        for channel_id in subscribed_channels:
+        for channel_id in subscribed_channels.values():
             channel = bot.get_channel(channel_id)
             await channel.send(message)
 
-# Initialize the last_post_link attribute
-check_for_new_posts.last_post_link = None
-
-# Define a subscribe command that adds or removes channels from the subscribed_channels list
+# Define a subscribe command that adds the current channel to the subscribed_channels dictionary
 @bot.command()
 async def subscribe(ctx):
-    channel_id = ctx.channel.id
-    if channel_id in subscribed_channels:
-        subscribed_channels.remove(channel_id)
-        await ctx.send('This channel has been unsubscribed from the frame.work blog feed.')
-    else:
-        subscribed_channels.append(channel_id)
-        await ctx.send('This channel has been subscribed to the frame.work blog feed.')
+    server_id = ctx.guild.id
+    subscribed_channels[server_id] = ctx.channel.id
+    await ctx.send('This channel has been subscribed to the example.com blog feed.')
+    save_subscribed_channels()
 
-# Start the task
-@bot.event
-async def on_ready():
-    print('Bot is ready.')
-    check_for_new_posts.start()
+# Define an unsubscribe command that removes the current channel from the subscribed_channels dictionary
+@bot.command()
+async def unsubscribe(ctx):
+    server_id = ctx.guild.id
+    if server_id in subscribed_channels:
+        del subscribed_channels[server_id]
+        await ctx.send('This channel has been unsubscribed from the example.com blog feed.')
+        save_subscribed_channels()
+    else:
+        await ctx.send('This channel is not currently subscribed to the example.com blog feed.')
+
+# Define a function to save the subscribed_channels dictionary to a file
+def save_subscribed_channels():
+    with open('subscribed_channels.json', 'w') as f:
+        json.dump(subscribed_channels, f)
+
+# Define a function to load the subscribed_channels dictionary from a file
+def load_subscribed_channels():
+    try:
+        with open('subscribed_channels.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+# Load the subscribed_channels dictionary from a file
+subscribed_channels = load_subscribed_channels()
+
+# Start the check_for_new_posts loop
+check_for_new_posts.start()
 
 # Run the bot
 bot.run(DISCORD_BOT_TOKEN)
+
+# Save the subscribed_channels dictionary to a file on shutdown
+save_subscribed_channels()
